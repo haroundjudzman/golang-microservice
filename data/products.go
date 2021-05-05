ace6c754d728976af3bc7471cbf7e3e0e2ce8b40
Haroun Djudzman <haroun.djudzman@gmail.com>
2021-05-05 23:57:55 +0700

    feat(data) add API data structure

---
+++ data/products.go
@@ -0,0 +1,37 @@
+package data
+
+import "time"
+
+// Product defines structure for API data
+// The products are burgers from Bob's Burgers
+type Product struct {
+	ID        int
+	Name      string
+	Season    int
+	Episode   int
+	Price     float32
+	CreatedOn string
+	UpdatedOn string
+	DeletedOn string
+}
+
+var productList = []*Product{
+	&Product{
+		ID:        1,
+		Name:      "New Bacon-ings Burger",
+		Season:    1,
+		Episode:   1,
+		Price:     9.40,
+		CreatedOn: time.Now().UTC().String(),
+		UpdatedOn: time.Now().UTC().String(),
+	},
+	&Product{
+		ID:        2,
+		Name:      "Egger Can't Be Cheesers Burger",
+		Season:    3,
+		Episode:   11,
+		Price:     8.40,
+		CreatedOn: time.Now().UTC().String(),
+		UpdatedOn: time.Now().UTC().String(),
+	},
+}
