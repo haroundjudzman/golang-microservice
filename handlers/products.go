9e7a4c3d377dae82e04c1ea79dce65a9e606c8d0
Haroun Djudzman <haroun.djudzman@gmail.com>
2021-05-10 23:04:38 +0700

    refactor(handler) change to gorilla mux router

--- handlers/products.go
+++ handlers/products.go
@@ -3,9 +3,9 @@ package handlers
 import (
 	"log"
 	"net/http"
-	"regexp"
 	"strconv"
 
+	"github.com/gorilla/mux"
 	"github.com/haroundjudzman/golang-microservice/data"
 )
 
@@ -18,51 +18,8 @@ func NewProducts(l *log.Logger) *Products {
 	return &Products{l}
 }
 
-// ServeHTTP is used such that Products satisfies the http.Handler interface
-func (p *Products) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	if r.Method == http.MethodGet {
-		p.getProducts(w, r)
-		return
-	}
-
-	if r.Method == http.MethodPost {
-		p.addProduct(w, r)
-		return
-	}
-
-	if r.Method == http.MethodPut {
-		// get the id in the URI
-		path := r.URL.Path
-		regex := regexp.MustCompile(`/([0-9]+)`)
-		matchGroup := regex.FindAllStringSubmatch(path, -1)
-
-		if len(matchGroup) != 1 {
-			http.Error(w, "Invalid URI", http.StatusBadRequest)
-			return
-		}
-
-		if len(matchGroup[0]) != 2 {
-			http.Error(w, "Invalid URI", http.StatusBadRequest)
-			return
-		}
-
-		idString := matchGroup[0][1]
-		id, err := strconv.Atoi(idString)
-		if err != nil {
-			http.Error(w, "Invalid URI", http.StatusBadRequest)
-			return
-		}
-
-		p.updateProduct(id, w, r)
-		return
-	}
-
-	// catch all
-	w.WriteHeader(http.StatusMethodNotAllowed)
-}
-
 // getProducts return all products from data store
-func (p *Products) getProducts(w http.ResponseWriter, r *http.Request) {
+func (p *Products) GetProducts(w http.ResponseWriter, r *http.Request) {
 	p.l.Println("Handling GET method")
 
 	productList := data.GetProducts()
@@ -73,7 +30,7 @@ func (p *Products) getProducts(w http.ResponseWriter, r *http.Request) {
 }
 
 // addProduct adds a product to data store
-func (p *Products) addProduct(w http.ResponseWriter, r *http.Request) {
+func (p *Products) AddProduct(w http.ResponseWriter, r *http.Request) {
 	p.l.Println("Handling POST method")
 
 	product := &data.Product{}
@@ -86,13 +43,21 @@ func (p *Products) addProduct(w http.ResponseWriter, r *http.Request) {
 }
 
 // updateProduct updates the product with given ID
-func (p *Products) updateProduct(id int, w http.ResponseWriter, r *http.Request) {
+func (p *Products) UpdateProduct(w http.ResponseWriter, r *http.Request) {
 	p.l.Println("Handling PUT method")
 
+	// Get the id param and convert to integer
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		http.Error(w, "Unable to convert id", http.StatusBadRequest)
+	}
+
 	product := &data.Product{}
-	err := product.FromJSON(r.Body)
+	err = product.FromJSON(r.Body)
 	if err != nil {
 		http.Error(w, "Unable to unmarshal json", http.StatusBadRequest)
+		return
 	}
 
 	err = data.UpdateProduct(id, product)
